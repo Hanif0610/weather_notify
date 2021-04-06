3f30f141d9a5a0d911e58447d64b877fa9a026bb
Hanif <marbling1293@naver.com>
2021-04-06 11:30:19 +0900

    [ADD] Weather api 불러오기

--- src/main/kotlin/alone/project/weather_notify/controller/WeatherController.kt
+++ src/main/kotlin/alone/project/weather_notify/controller/WeatherController.kt
@@ -1,9 +1,20 @@
 package alone.project.weather_notify.controller
 
+import alone.project.weather_notify.dto.WeatherRequest
+import alone.project.weather_notify.service.WeatherService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/weather")
-class WeatherController {
+class WeatherController(
+    @Autowired private val weatherService: WeatherService
+) {
+
+    @GetMapping
+    fun getData(weather: WeatherRequest): Any? {
+        return weatherService.getData(weather)
+    }
 }
---
+++ src/main/kotlin/alone/project/weather_notify/service/WeatherService.kt
@@ -0,0 +1,49 @@
+package alone.project.weather_notify.service
+
+import alone.project.weather_notify.dto.DataResponse
+import alone.project.weather_notify.dto.WeatherRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class WeatherService{
+
+    fun getData(weather: WeatherRequest): Any? {
+        val date: String = LocalDate.now().minusDays(1).toString().split("-").joinToString("")
+
+        val key = "egDfenHknpZmGHHRQMji%2B9Qx99SYWf9xPzOkmz%2BErohCIFA8YI%2B1XNNKWikdd7hmQ9d8TIY3QAUWiN2zuHInCg%3D%3D"
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?" +
+                    "serviceKey=$key&numOfRows=93&dataType=JSON&" +
+                    "base_date=$date&base_time=2000&nx=${weather.nx}&ny=${weather.ny}").build()
+        val response = client.newCall(request).execute()
+
+        if(response.isSuccessful) {
+            val body = response.body!!.string()
+            val objectMapper = ObjectMapper().registerModule(KotlinModule())
+
+            val jsonParser = JSONParser()
+            val jsonObject: JSONObject = jsonParser.parse(body) as JSONObject
+            val jsonResponse: JSONObject = jsonObject["response"] as JSONObject
+            val jsonBody: JSONObject = jsonResponse["body"] as JSONObject
+            val items: JSONObject = jsonBody["items"] as JSONObject
+            val item: JSONArray = items["item"] as JSONArray
+
+            val list: MutableList<DataResponse> = ArrayList()
+            item.forEach {
+                list.add(objectMapper.readValue(it.toString(), DataResponse::class.java))
+            }
+            return list
+        }
+        return ""
+    }
+}
