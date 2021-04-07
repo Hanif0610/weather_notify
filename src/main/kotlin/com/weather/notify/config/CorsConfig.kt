9bfdae2fe41d1b08f4eeb5edb544eb782725aa9c
Hanif <marbling1293@naver.com>
2021-04-07 14:02:46 +0900

    [FIX] swagger

--- src/main/kotlin/com/weather/notify/config/CorsConfig.kt
+++ src/main/kotlin/com/weather/notify/config/CorsConfig.kt
@@ -2,26 +2,15 @@ package com.weather.notify.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedHeaders("*")
             .allowedMethods("*")
-    }
-
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/")
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/")
     }
 }
