fa4f7fc7784db4afe8d3f4edc76a6b15f88bdc84
Hanif <marbling1293@naver.com>
2021-04-07 16:30:42 +0900

    [ADD] User

---
+++ src/main/kotlin/com/weather/notify/domain/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.weather.notify.domain.repository
+
+import com.weather.notify.domain.entity.User
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository: MongoRepository<User, Int> {
+    fun findByEmail(email: String): User?
+}
