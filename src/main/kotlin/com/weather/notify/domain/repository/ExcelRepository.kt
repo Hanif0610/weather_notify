8a779fa3c95ece44c0ce65e65ec57af2a9d286a8
Hanif <marbling1293@naver.com>
2021-04-14 17:53:13 +0900

    [FIX] mongoRepository -> JpaRepository

--- src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
+++ src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
@@ -1,8 +1,8 @@
 package com.weather.notify.domain.repository
 
 import com.weather.notify.domain.entity.Location
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ExcelRepository: MongoRepository<Location, String>
+interface ExcelRepository: JpaRepository<Location, String>
