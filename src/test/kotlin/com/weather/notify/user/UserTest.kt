7f98c1856e8a98c241580ad1a5be1b09341c0931
Hanif <marbling1293@naver.com>
2021-04-19 13:50:19 +0900

    [ADD] update name test

--- src/test/kotlin/com/weather/notify/user/UserTest.kt
+++ src/test/kotlin/com/weather/notify/user/UserTest.kt
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.weather.notify.domain.entity.User
 import com.weather.notify.domain.repository.UserRepository
-import com.weather.notify.dto.JoinRequest
-import com.weather.notify.dto.LoginRequest
-import com.weather.notify.dto.ProfileResponse
-import com.weather.notify.dto.TokenResponse
+import com.weather.notify.dto.*
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -20,8 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
@@ -64,10 +61,10 @@ class UserTest(
             .content(objectMapper.writeValueAsString(user)))
             .andExpect(status().isOk)
 
-        userRepository.findByEmail(user.email).let {
-            assertNotNull(it?.email)
-            assertNotNull(it?.name)
-            assertNotNull(it?.password)
+        userRepository.findByEmail(user.email)?.let {
+            assertNotNull(it.email)
+            assertNotNull(it.name)
+            assertNotNull(it.password)
         }
     }
 
@@ -88,6 +85,29 @@ class UserTest(
 
         assertNotNull(response.email)
         assertNotNull(response.name)
+    }
+
+    @Test
+    @DisplayName(value = "프로필 이름 수정")
+    fun updateName() {
+        userRepository.save(user)
+
+        val token = getToken(post("/auth"), login)
+        val updateName = UpdateNameRequest(
+            name = "배현호"
+        )
+
+        mock.perform(
+            put("/user/name")
+                .header("Authorization", "Bearer $token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateName)))
+            .andExpect(status().isOk)
+
+//        userRepository.findByEmail(user.email)?.let {
+//            assertNotNull(it.name)
+//            assertEquals(it.name, updateName.name)
+//        }
     }
 
     private fun getToken(request: MockHttpServletRequestBuilder, obj: Any? = null): String {
