6403cbc489fe07f5bfec869ab7f4540d44f00282
Hanif <marbling1293@naver.com>
2021-05-26 15:12:55 +0900

    [ADD] Deep1에 해당하는 모든 데이터 불러오기

--- src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
+++ src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ExcelRepository: MongoRepository<Location, String>
+interface ExcelRepository: MongoRepository<Location, String> {
+    fun findAllByDeep1(deep1: String): List<Location>
+}
