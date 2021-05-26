d2499ba680b681317558d72de9378714d1b43215
Hanif <marbling1293@naver.com>
2021-05-26 15:21:11 +0900

    [ADD] Deep1과 Deep2에 해당하는 Deep3 가져오기

--- src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
+++ src/main/kotlin/com/weather/notify/domain/repository/ExcelRepository.kt
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface ExcelRepository: MongoRepository<Location, String> {
     fun findAllByDeep1(deep1: String): List<Location>
+    fun findAllByDeep1AndDeep2(deep1: String, deep2: String): List<Location>
 }
